7334518f89d69dbab313a8e2451fe203e2fde891
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-09 15:03:46 -0500

    first commit

---
+++ src/main/java/com/eescovitchr/laboratory_api/LaboratoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.eescovitchr.laboratory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eescovitchr/laboratory_api/LaboratoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eescovitchr.laboratory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
